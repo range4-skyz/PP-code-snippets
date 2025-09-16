774b96022f729b07f15fd2133147af5efab5a1bb
range4-skyz <sxy2367221250@163.com>
2025-09-16 14:31:13 +0800

    feat: add helpful comments

--- src/base_macros.h
+++ src/base_macros.h
@@ -24,11 +24,22 @@
 
 //// ---- References:
 // - https://gcc.gnu.org/onlinedocs/cpp/Macros.html
+// - https://learn.microsoft.com/en-us/cpp/preprocessor/c-cpp-preprocessor-reference?view=msvc-170
+// - https://learn.microsoft.com/en-us/cpp/preprocessor/preprocessor-operators?view=msvc-170
 //
 // - https://www.ashn.dev/blog/2020-01-06-c-array-length.html
+// - https://www.scs.stanford.edu/~dm/blog/va-opt.html
+// - https://www.dominikgrabiec.com/posts/2023/02/28/making_a_flexible_assert.html
 
 //// ---- Godbolt Tests:
-// - GCC/Clang/MinGW/MSVC(x86-64): https://godbolt.org/z/c67hx8757
+// - C
+//      - GCC/Clang(x86-64) : https://godbolt.org/z/TK1odbP9z [ -std=c99 / -std=c11 ]
+//      - MinGW W64         : https://godbolt.org/z/bPsEz1f9h [ -std=c99 / -std=c11 ]
+//      - MSVC(x86/x64)     : https://godbolt.org/z/4efE1c4nv [ /W3 /std:c11 ]
+// - C++
+//      - GCC/Clang(x86-64) : https://godbolt.org/z/Yxrx9e1eb [ -std=c++98 / -std=c++11 ]
+//      - MinGW W64         : https://godbolt.org/z/oqP5zzn9K [ -std=c++98 / -std=c++11 ]
+//      - MSVC(x86/x64)     : https://godbolt.org/z/Tj7e6vsM5 [ /W3 /std:c++17 ]
 
 //// ---- Key Points:
 //
@@ -53,25 +64,53 @@
 #define TOSTRING_IMPL(x) #x
 #define TOSTRING(x) TOSTRING_IMPL( x )
 
-/* Merge two tokens into one while expanding macros */
+/* Merging or combining into one while expanding macros */
 #define CONCAT_IMPL(l, r) l ## r
 #define CONCAT(l, r) CONCAT_IMPL( l, r )
 
-/* Merge more two tokens into one while expanding macros */
+/* Merging or combining more than two tokens into one while expanding macros */
 #define CONCAT_N_IMPL(l, ...) l ## __VA_ARGS__
 #define CONCAT_N(l, ...) CONCAT_N_IMPL( l, __VA_ARGS__ )
 
-#define L_PAREN (
-#define R_PAREN )
-#define LR_PARENS ()
+/* For expanding an object-like macro into a function-like macro
+- Usage:
++-----------------------------------------------------------------------+
+|    #define ID(arg) arg                                                |
+|    ID(ID)(ID)(X)   // => ID(ID)(X)                                    |
+|                                                                       |
+|    #define LPAREN (                                                   |
+|    #define ID2(arg) arg                                               |
+|    ID(ID2)(ID)(X)        // => X                                      |
+|    ID(ID2 LPAREN)ID)(X)  // => X                                      |
+|    ID(ID2 LPAREN ID))(X) // => ID(X)                                  |
+|    ID(ID2 (ID))(X)       // => ID(X)                                  |
+|                                                                       |
+|    #define ID(arg) arg                                                |
+|    #define PARENS () // Note space before (), so object-like macro    |
+|    #define F_AGAIN() F                                                |
+|    #define F() f F_AGAIN PARENS()                                     |
+|                                                                       |
+|    F()         // => f F_AGAIN ()()                                   |
+|    ID(F())     // => f f F_AGAIN ()()                                 |
+|    ID(ID(F())) // => f f f F_AGAIN ()()                               |
++-----------------------------------------------------------------------+ */
+#define LPAREN (
+#define RPAREN )
+#define PARENS ()
+// #define L_PAREN (
+// #define R_PAREN )
+// #define LR_PARENS ()
 
-/* Implement nested macro calls using argument pre-scanning
- * Rescan once time */
+/* Implement nested macro calls by pre-scanning the arguments.
+   In other words, add another layer of indirection to force the preprocessor to rescan and reconsider further expansion.
+ * Rescanning once */
 #define EXPAND(x) x
 #define EXPAND_N(...) __VA_ARGS__
 
-/* Implement nested macro calls using argument pre-scanning
-   Rescan multiple times */
+/* Implement nested macro calls by pre-scanning the arguments.
+   In other words, add another layer of indirection to force the preprocessor to rescan and reconsider further expansion.
+ * Rescanning multiple times, it allows handling deeper levels of recursive macros, but it can easily cause
+   macro expansions to grow exponentially and may result in intermediate macros being rescanned repeatedly. */
 #define EXPAND_N_SUPER(...)   EXPAND_S1_N( EXPAND_S1_N( EXPAND_S1_N( EXPAND_S1_N( __VA_ARGS__ ) ) ) )
 #define EXPAND_S1_N(...)      EXPAND_S2_N( EXPAND_S2_N( EXPAND_S2_N( EXPAND_S2_N( __VA_ARGS__ ) ) ) )
 #define EXPAND_S2_N(...)      EXPAND_S3_N( EXPAND_S3_N( EXPAND_S3_N( EXPAND_S3_N( __VA_ARGS__ ) ) ) )
